4d442cb1594b05212b10e254c58c68bee32035a4
rodrigorenteriatx <rodrigorenteriatx@gmail.com>
2025-07-23 20:18:42 -0700

    ADDED CHECKS to config.sh

--- config.sh
+++ config.sh
@@ -8,3 +8,21 @@ USE_CENTRIFY=true
 USE_AUTOFS=true
 
 USE_SSSD=false
+
+check_PKI () {
+    if [ "$USE_PKI" == "false" ]; then
+        echo -e "NOT APPLICABLE\nNOT USING PKI BASED AUTHENTICATION"
+        finding=2
+        return $finding
+    fi
+    return 0
+}
+
+check_SSSD () {
+    if [ "$USE_PKI" == "false" ]; then
+        echo -e "NOT APPLICABLE\nNOT USING SSSD BASED AUTHENTICATION"
+        finding=2
+        return $finding
+    fi
+    return 0
+}
