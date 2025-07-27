28e5391f1e646c753e5009deb7e7ef2d95c35e76
rodrigorenteriatx <rodrigorenteriatx@gmail.com>
2025-07-27 14:17:41 -0700

    ADDED CHECKS to config.sh

--- config.sh
+++ config.sh
@@ -9,16 +9,27 @@ else
 fi
 
 #CHECK IF SSSD IS BEING ACTIVELY USED
-if systemctl is-enabled sssd && systemctl is-active; then
+if systemctl is-enabled sssd && systemctl is-active sssd; then
     USE_SSSD=true
     #Rule 1041 - scripts/v-230274
 else
     USE_SSSD=false
 fi
 
-USE_CENTRIFY=true
+if systemctl is-enabled centrifydc && systemctl is-active centrifydc; then
+    USE_CENTRIFY=true
+    #CANT USE SSSD
+    USE_SSSD=false
+else
+    USE_CENTRIFY=false
+fi
 #
-USE_AUTOFS=true
+
+if systemctl is-enabled autofs && systemctl is-active autofs; then
+    USE_AUTOFS=true
+else
+    USE_AUTOFS=false
+fi
 
 
 
@@ -32,8 +43,10 @@ check_PKI () {
 }
 
 check_SSSD () {
-    if [ "$USE_PKI" == "false" ]; then
+    if [ "$USE_SSSD" == "false" ]; then
         echo "NOT APPLICABLE: NOT USING SSSD BASED AUTHENTICATION"
+        if [ "$USE_CENTRIFY" == "true" ]; then
+            echo "USING CENTRIFY FOR AUTHENTICATION"
         return 2
     fi
     return 0
@@ -49,3 +62,20 @@ USE_AUTOFS=$USE_AUTOFS
 USE_NFS_HOME=$USE_NFS_HOME
 IS_GUI_INSTALLED=$IS_GUI_INSTALLED
 EOF
+
+
+# In DISA STIG context:
+
+#     PKI (Public Key Infrastructure) authentication means using smartcards (e.g., CAC/PIV) for logins.
+
+#     MFA (Multi-Factor Authentication) is a broader category that includes:
+
+#         Something you know (e.g., password)
+
+#         Something you have (e.g., smartcard/token)
+
+#         Something you are (biometrics)
+
+# So:
+
+#     All PKI-based logins are MFA, but not all MFA setups use PKI
